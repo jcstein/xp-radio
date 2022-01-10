db378df27a02ef3e7e6d22d1060c9c62634fe506
joshcs.eth <46639943+jcstein@users.noreply.github.com>
2022-01-10 10:44:07 -0500

    🎚

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,18 +1,20 @@
-import { ColorModeScript } from "@chakra-ui/react"
-import NextDocument, { Html, Head, Main, NextScript } from "next/document"
-import theme from "../theme"
+import { ColorModeScript } from "@chakra-ui/react";
+import NextDocument, { Html, Head, Main, NextScript } from "next/document";
+import theme from "../theme";
 
 export default class Document extends NextDocument {
   render() {
     return (
       <Html lang="en">
-        <Head />
+        <Head>
+          <script src="/public/theme.js"></script>
+        </Head>
         <body>
           <ColorModeScript initialColorMode={theme.config.initialColorMode} />
           <Main />
           <NextScript />
         </body>
       </Html>
-    )
+    );
   }
 }
